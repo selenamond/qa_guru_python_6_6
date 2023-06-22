ffbb484e90390a028ce553c01360cbed1624ae5b
elenautkina <selenamondeu@gmail.com>
2023-06-22 21:38:35 +0300

    add changes to last test

--- homework.py
+++ homework.py
@@ -100,24 +100,22 @@ def test_readable_function():
 
 def rename_function(func, *arg):
     changed_func_name = func.replace('_', ' ').title()
-    return changed_func_name + ' [' + ', '.join(arg) + ']'
-
+    result = changed_func_name + ' [' + ', '.join(arg) + ']'
+    print(result)
+    return result
 
 def open_browser(browser_name):
-    actual_result = None
     actual_result = rename_function(open_browser.__name__, browser_name)
 
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
     actual_result = rename_function(go_to_companyname_homepage.__name__, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
     actual_result = rename_function(find_registration_button_on_login_page.__name__,
                                     page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
